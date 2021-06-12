f44e8bdae242cb3fdad2cafa66735a86edd48b93
Aritra Kar <aritrakar2002@gmail.com>
2021-06-12 13:10:12 +0530

    Create GreetMe.java

---
+++ 7,8 kyu/GreetMe.java
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/535474308bb336c9980006f2
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+public class GreetMe{
+  public static String greet(String name){
+    name = name.toLowerCase();
+    String s = name.substring(0,1).toUpperCase() + name.substring(1, name.length());
+    return "Hello "+s+"!";
+  }
+}
