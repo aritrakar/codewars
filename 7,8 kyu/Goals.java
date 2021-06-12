9fd55d6b466916dc6e7569275f17c15d0b3a1e75
Aritra Kar <aritrakar2002@gmail.com>
2021-06-12 13:09:31 +0530

    Create Goals.java

---
+++ 7,8 kyu/Goals.java
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/55ca77fa094a2af31f00002a
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+class Goals {
+  public static int laLigaGoals=43;
+  public static int championsLeagueGoals=10;
+  public static int copaDelReyGoals=5;
+  public static int totalGoals = laLigaGoals+championsLeagueGoals+copaDelReyGoals;
+}
