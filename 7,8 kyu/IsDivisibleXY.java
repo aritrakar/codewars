d85f4b6870ade3ee1b26e4b43a52f810e9b7a987
Aritra Kar <aritrakar2002@gmail.com>
2021-06-12 17:49:54 +0530

    Create IsDivisibleXY.java

---
+++ 7,8 kyu/IsDivisibleXY.java
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5545f109004975ea66000086
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
+
+public class DivisibleNb {
+  public static boolean isDivisible(long n, long x, long y) {
+    // your code
+    return n%x==0 && n%y==0 ? true: false;
+  }
+}
